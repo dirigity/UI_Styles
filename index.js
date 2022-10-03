cc5512e7d49d8822d0dfe1bf284b1fe7aba0997f
Jaime González Fábregas <jaimegonzalezfabregas@gmail.com>
2022-10-03 18:23:38 +0200

    movile compatibility n calculator funcitionality n sevseg font

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let themes = ["structure", "et", "e", "pd", "md", "fd", "n", "g"];
 
 function hide_all() {
     themes.forEach(th => {
+        document.getElementById(th).classList = "" // setted
         document.getElementById("tb_" + th).style.display = "none";
     });
 }
@@ -9,19 +10,68 @@ function hide_all() {
 themes.forEach(th => {
     document.getElementById(th).addEventListener("click", () => {
         hide_all();
+        document.getElementById(th).classList = "setted"
         document.getElementById("tb_" + th).style.display = "block";
         window.localStorage.setItem("active", th)
     })
 });
 
+
+
 if (window.localStorage.getItem("active")) {
 
     hide_all();
-    document.getElementById("tb_" + window.localStorage.getItem("active")).style.display = "block";
+    let th = window.localStorage.getItem("active")
+    document.getElementById(th).classList = "setted"
+    document.getElementById("tb_" + th).style.display = "block";
 
 } else {
 
     hide_all();
+    document.getElementById(themes[0]).classList = "setted"
     document.getElementById("tb_" + themes[0]).style.display = "block";
 
+}
+
+let display_text = "";
+
+document.querySelectorAll(".angry-grid").forEach((c) => {
+    c.childNodes.forEach((e) => {
+        e.addEventListener("click", () => {
+
+            let key = e.innerHTML;
+            if (key == "DEL") {
+                display_text = display_text.slice(0, -1);
+                update_display()
+            } else if (key == "AC") {
+                display_text = ""
+                update_display()
+            } else if (key == "=") {
+                if (display_text == "") {
+                    display_text = "ERR"
+                    update_display()
+                    display_text = ""
+                } else {
+                    try {
+                        display_text = ("" + eval(display_text.replace("x", "*").replaceAll(",", "."))).replace(".", ",")
+                        update_display()
+                    } catch (error) {
+                        display_text = "ERR"
+                        update_display()
+                        display_text = ""
+                    }
+                }
+
+            } else {
+                display_text += key;
+                update_display()
+            }
+        })
+    })
+})
+
+function update_display() {
+    document.querySelectorAll("display").forEach((c) => {
+        c.innerHTML = display_text
+    })
 }
